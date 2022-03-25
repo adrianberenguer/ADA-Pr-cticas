f148b4bafde6a3ccdd702cbf6f4520dfd262b7d6
berenadr <berenadr@cvut.cz>
2022-03-25 20:39:21 +0100

    asdg

--- Práctica6/met.cc
+++ Práctica6/met.cc
@@ -11,34 +11,110 @@
 using namespace std;
 
 // Leer el fichero de entrada y guardar sus valores
-bool leerFichero() {
+bool leerFichero(string inputFile, int &n, int &m, vector<double> &capacidades, vector<double> &distancias) {
+    //n m
+    //ci (capacidad de almacenamiento)
+    //di (distancia al nodo origen)
+    vector<double> temporal;
 
+    ifstream file;
+    file.open(inputFile.c_str(), ios::in);
+
+    if(file.is_open())
+    {
+        string linea = "";
+        double number = -1;
+
+        while(getline(file, linea)) {
+            stringstream ss(linea);
+
+            if(n == -1 && m == -1) {
+                ss >> n;
+                ss >> m;
+            }
+            else {
+                while(ss >> number) {
+                    temporal.push_back(number);
+                }
+
+                if(capacidades.size() == 0) {
+                    capacidades = temporal;
+                    temporal.clear();
+                }
+                else if(distancias.size() == 0) {
+                    distancias = temporal;
+                    temporal.clear();
+                }
+            }
+        }
+
+        file.close();
+        return true;
+    }
+    else {
+        cerr << "ERROR: can't open file: " << inputFile << endl;
+        cout << "Usage:" << endl << "$met [-t] [--ignore-naive] -f file" << endl;
+        return false;
+    }
 }
 
 // Comprobar los argumentos
-bool compruebaArgs() {
+bool compruebaArgs(int argc, char *argv[], string &inputFile, bool &ignoraSinAlmIng, bool &tablas) {
+    //met [ -t] [ --ignore-naive] -f fichero entrada
+    if(argc == 1) {
+        return false;
+    }
 
+    for(int i = 1; i < argc; i++)
+    {
+        if((string)argv[i] == "--ignore-naive") {
+            ignoraSinAlmIng = false;
+        }
+        else if((string)argv[i] == "-t") {
+            tablas = true;
+        }
+        else if ((string)argv[i] == "-f") {
+            if(i != argc - 1) {
+                inputFile = argv[i+1];
+                i++;                   
+            }
+            else {
+                cerr << "ERROR: missing filename." << endl;
+                return false;
+            }   
+        }
+        else {
+            cerr << "ERROR: unknown option " << argv[i] << "." << endl;
+            return false;
+        }
+    }
+
+    return true;
 }
 
 // Recursivo sin almacén(versión ingenua)
-void met_naive() {
+double met_naive(int n, int m, vector<double> &capacidades, vector<double> &distancias) {
 
+    return 0;
 }
 
 // Recursivo con almacén
-void met_memo() {
+double met_memo(int n, int m, vector<double> &capacidades, vector<double> &distancias) {
 
+    return 0;
 }
 
 // Iterativo con almacén que hace uso de una tabla para 
 // almacenar resultados intermedios
-void met_it_matrix() {
+double met_it_matrix(int n, int m, vector<double> &capacidades, vector<double> &distancias) {
 
+    return 0;
 }
 
 // Iterativo con almacén con complejidad espacial mejorada
-void met_it_vector() {
+double met_it_vector(int n, int m, vector<double> &capacidades, vector<double> &distancias) {
 
+    return 0;
 }
 
 // Tabla de encaminamiento
@@ -48,6 +124,25 @@ void met_parser() {
 
 // Main
 int main(int argc, char *argv[]) {
+    string inputFile = "";
+    bool ignoraSinAlmIng = true, tablas = false;
+
+    if(!compruebaArgs(argc, argv, inputFile, ignoraSinAlmIng, tablas))
+    {
+        cout << "Usage:" << endl << "met [-t] [--ignore-naive] -f file" << endl;
+    }
+    else
+    {
+        int n = -1, m = -1;
+        vector<double> capacidades, distancias;
+
+        if(!leerFichero(inputFile, n, m, capacidades, distancias)) {
+            return 0;
+        }
+
+        
+
+    }
 
 
     return 0;
